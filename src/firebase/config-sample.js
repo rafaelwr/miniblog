7fd65972021c63940a1d62f95b361671db022a28
Teste <teste@saleplus.net.br>
2022-10-14 23:55:48 -0300

    Instalado o Firebase.

---
+++ src/firebase/config-sample.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+// Preencher com os dados do Firebase
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
