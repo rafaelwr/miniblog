4c4c9442a07bf77b3bd289189e296a4e5c4983e9
Teste <teste@saleplus.net.br>
2022-10-21 23:46:08 -0300

    Criação do hook useAuthentication.

--- src/firebase/config-sample.js
+++ src/firebase/config-sample.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase";
+import { getFirestore } from "firebase/firestore";
 
 // Preencher com os dados do Firebase
 const firebaseConfig = {
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -2,14 +2,17 @@ import styles from './Register.module.css';
 
 import { useState, useEffect } from 'react';
 
+import { useAuthentication } from '../../hooks/useAuthentication';
+
 const Register = () => {
     const [ displayName, setDisplayName ] = useState('');
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
     const [ confirmPassword, setConfirmPassword ] = useState('');
     const [ error, setError ] = useState('');
+    const { createUser, error: authError, loading } = useAuthentication();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         setError('');
@@ -26,8 +29,16 @@ const Register = () => {
             password
         }
 
-        console.log(user);
+        const res = await createUser(user);
+
+        console.log(res);
     }
+
+    useEffect(() => {
+        if(authError) {
+            setError(authError);
+        }
+    }, [ authError ]);
 
   return (
     <div className={styles.register}>
@@ -78,8 +89,9 @@ const Register = () => {
                     onChange={(e) => setConfirmPassword(e.target.value)}
                 />
             </label>
-            <button type="submit" className="btn">Cadastrar</button>
-            {error && <p className="error">{error}</p>}
+            { !loading && <button type="submit" className="btn">Cadastrar</button> }
+            { loading && <button type="submit" className="btn" disabled>Aguarde...</button> }
+            { error && <p className="error">{error}</p> }
         </form>
     </div>
   )
