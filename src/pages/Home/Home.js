7f3aa30054455681ce6d5b2bf6ec02217be4a2c3
Teste <teste@saleplus.net.br>
2022-10-23 23:45:10 -0300

    Ajustes da Home com as consultas de posts.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,12 +4,14 @@ import styles from './Home.module.css';
 // Hooks
 import { useNavigate, Link } from 'react-router-dom';
 import { useState } from 'react';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
 
 // Components
+import PostDetail from '../../components/PostDetail';
 
 const Home = () => {
   const [ query, setQuery ] = useState('');
-  const [ posts ] = useState([]);
+  const { documents: posts, loading } = useFetchDocuments('posts');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,7 +28,10 @@ const Home = () => {
           />
           <button className="btn btn-dark">Pesquisar</button>
         </form>
-        <h1>Posts</h1>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => (
+          <PostDetail key={post.id} post={post}/>
+        ))}
         {posts && posts.length === 0 &&(
           <div className={styles.noposts}>
             <p>Não foram encontrados posts.</p>
